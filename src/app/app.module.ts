301c796a1a37fd0e6dfcd0e472f2d9d325dbb04b
Jerome Flaherty <jayromulus.f@gmail.com>
2019-07-08 16:58:53 -0400

    dev update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,7 +29,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatCheckboxModule,
     MatMenuModule,
     MatCardModule,
-    MatGridListModule
+    MatGridListModule,
     FormsModule,
     HttpClientModule
   ],
